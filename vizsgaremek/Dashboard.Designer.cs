04ee25b9adcf8a216ae69c93b50ca86fd0250902
Norbi1027 <sandornorbi7@gmail.com>
2023-09-21 13:49:10 +0200

    design

--- vizsgaremek/Dashboard.Designer.cs
+++ vizsgaremek/Dashboard.Designer.cs
@@ -34,24 +34,24 @@
             datum_tb = new TextBox();
             osszeg_tb = new TextBox();
             label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
             insert_btn = new Button();
             update_brn = new Button();
             delete_btn = new Button();
             dataGridView1 = new DataGridView();
             search_btn = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // rendeles_id_tb
             // 
-            rendeles_id_tb.Location = new Point(240, 94);
+            rendeles_id_tb.Location = new Point(240, 106);
             rendeles_id_tb.Multiline = true;
             rendeles_id_tb.Name = "rendeles_id_tb";
-            rendeles_id_tb.Size = new Size(280, 37);
+            rendeles_id_tb.Size = new Size(280, 25);
             rendeles_id_tb.TabIndex = 0;
             // 
             // user_id_tb
@@ -59,77 +59,42 @@
             user_id_tb.Location = new Point(240, 154);
             user_id_tb.Multiline = true;
             user_id_tb.Name = "user_id_tb";
-            user_id_tb.Size = new Size(280, 37);
+            user_id_tb.Size = new Size(280, 25);
             user_id_tb.TabIndex = 1;
             // 
             // skin_id_tb
             // 
-            skin_id_tb.Location = new Point(240, 211);
+            skin_id_tb.Location = new Point(240, 201);
             skin_id_tb.Multiline = true;
             skin_id_tb.Name = "skin_id_tb";
-            skin_id_tb.Size = new Size(280, 37);
+            skin_id_tb.Size = new Size(280, 25);
             skin_id_tb.TabIndex = 2;
             // 
             // datum_tb
             // 
-            datum_tb.Location = new Point(240, 267);
+            datum_tb.Location = new Point(240, 248);
             datum_tb.Multiline = true;
             datum_tb.Name = "datum_tb";
-            datum_tb.Size = new Size(280, 37);
+            datum_tb.Size = new Size(280, 20);
             datum_tb.TabIndex = 3;
             // 
             // osszeg_tb
             // 
-            osszeg_tb.Location = new Point(240, 322);
+            osszeg_tb.Location = new Point(240, 287);
             osszeg_tb.Multiline = true;
             osszeg_tb.Name = "osszeg_tb";
-            osszeg_tb.Size = new Size(280, 37);
+            osszeg_tb.Size = new Size(280, 20);
             osszeg_tb.TabIndex = 4;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(157, 107);
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(56, 106);
             label1.Name = "label1";
-            label1.Size = new Size(66, 15);
+            label1.Size = new Size(178, 25);
             label1.TabIndex = 5;
-            label1.Text = "rendeles_id";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(168, 157);
-            label2.Name = "label2";
-            label2.Size = new Size(44, 15);
-            label2.TabIndex = 6;
-            label2.Text = "user_id";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(157, 214);
-            label3.Name = "label3";
-            label3.Size = new Size(43, 15);
-            label3.TabIndex = 7;
-            label3.Text = "skin_id";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(157, 270);
-            label4.Name = "label4";
-            label4.Size = new Size(42, 15);
-            label4.TabIndex = 8;
-            label4.Text = "dátum";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(146, 325);
-            label5.Name = "label5";
-            label5.Size = new Size(42, 15);
-            label5.TabIndex = 9;
-            label5.Text = "összeg";
+            label1.Text = "Rendelés azonosító:";
             // 
             // insert_btn
             // 
@@ -180,20 +145,60 @@
             search_btn.UseVisualStyleBackColor = true;
             search_btn.Click += search_btn_Click;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(32, 154);
+            label2.Name = "label2";
+            label2.Size = new Size(202, 25);
+            label2.TabIndex = 15;
+            label2.Text = "Felhasználó azonosító:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(69, 201);
+            label3.Name = "label3";
+            label3.Size = new Size(165, 25);
+            label3.TabIndex = 16;
+            label3.Text = "Kinézet azonosító:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(162, 248);
+            label4.Name = "label4";
+            label4.Size = new Size(72, 25);
+            label4.TabIndex = 17;
+            label4.Text = "Dátum:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(158, 282);
+            label5.Name = "label5";
+            label5.Size = new Size(76, 25);
+            label5.TabIndex = 18;
+            label5.Text = "Összeg:";
+            // 
             // Dashboard
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 605);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(search_btn);
             Controls.Add(dataGridView1);
             Controls.Add(delete_btn);
             Controls.Add(update_brn);
             Controls.Add(insert_btn);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(osszeg_tb);
             Controls.Add(datum_tb);
@@ -215,14 +220,14 @@
         private TextBox datum_tb;
         private TextBox osszeg_tb;
         private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
         private Button insert_btn;
         private Button update_brn;
         private Button delete_btn;
         private DataGridView dataGridView1;
         private Button search_btn;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
--- vizsgaremek/Form1.cs
+++ vizsgaremek/Form1.cs
@@ -13,44 +13,44 @@ namespace vizsgaremek
         private string sql;
         private void login_Click(object sender, EventArgs e)
         {
-            
+
             string mysqlconn = "server=127.0.0.1;user=root;database=login;password=";
             MySqlConnection mySqlConnection = new MySqlConnection(mysqlconn);
 
             string username = username_textbox.Text.ToString();
             string password = password_textbox.Text.ToString();
-             if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
-             {
-                 MessageBox.Show("üres mezõ!");
-             }
-             else
-             {
-                 mySqlConnection.Open();
-                 MySqlCommand mySqlCommand = new MySqlCommand("select * from login_test", mySqlConnection);
-                 MySqlDataReader reader = mySqlCommand.ExecuteReader();
-                 while (reader.Read())
-                 {
-                     if (username.Equals(reader.GetString("username")) && password.Equals(reader.GetString("password")))
-                     {
-                         
-                         Dashboard dsb = new Dashboard();
-                         Form1 form1 = new Form1();
-                         dsb.Show();
+            if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("üres mezõ!");
+            }
+            else
+            {
+                mySqlConnection.Open();
+                MySqlCommand mySqlCommand = new MySqlCommand("select * from login_test", mySqlConnection);
+                MySqlDataReader reader = mySqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (username.Equals(reader.GetString("username")) && password.Equals(reader.GetString("password")))
+                    {
+
+                        Dashboard dsb = new Dashboard();
+                        Form1 form1 = new Form1();
+                        dsb.Show();
                         form1.Close();
                         MessageBox.Show("Belépés sikeres!");
                         break;
                     }
-                     else
-                     {
-                         MessageBox.Show("Sikertelen bejelentkezés!");
+                    else
+                    {
+                        MessageBox.Show("Sikertelen bejelentkezés!");
                         break;
-                     }
+                    }
 
-                 }
-                 mySqlConnection.Close();
-             }
+                }
+                mySqlConnection.Close();
+            }
 
-           
+
 
         }
     }
